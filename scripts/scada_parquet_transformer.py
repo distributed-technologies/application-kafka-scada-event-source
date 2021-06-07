797ab2bf761d658371887931a481c67ab1ce2b33
Jacob Crawford <jacobcrawford1@hotmail.com>
2021-06-07 09:14:27 +0000

    Upgrading to make a line read offset.

--- scripts/scada_parquet_transformer.py
+++ scripts/scada_parquet_transformer.py
@@ -12,15 +12,21 @@ broker=str(sys.argv[1])
 consume_topic=str(sys.argv[2])
 produce_topic=str(sys.argv[3])
 consume_interval=int(sys.argv[4])
+topic_line_offset=str(sys.argv[5])
 consumer_group=None
 
-if len(sys.argv)>3:
-      consumer_group=str(sys.argv[5])
+if len(sys.argv)>5:
+      consumer_group=str(sys.argv[6])
 
 consumer = KafkaConsumer(
      consume_topic,
      bootstrap_servers=[broker],
      auto_offset_reset='earliest',
+     group_id=consumer_group)
+
+offset_line_consumer = KafkaConsumer(
+     topic_line_offset,
+     bootstrap_servers=[broker],
      group_id=consumer_group)
 
 producer = KafkaProducer(bootstrap_servers=[broker],
@@ -29,17 +35,31 @@ producer = KafkaProducer(bootstrap_servers=[broker],
 print(f"Contacted broker: {broker}")
 print(f"Started consuming from topic: {consume_topic}")
 print(f"Started producing on topic: {produce_topic}")
+
+
+# Fetch the latest message in the topic
+for message in offset_line_consumer:
+      line_number = message.value
+      break
+
+
 for message in consumer:
     # Read the kafka record as raw bytes and transform to a pandas dataframe 
     reader = pa.BufferReader(message.value)
     df = pq.read_table(reader).to_pandas() 
     lines_in_file = len(df)
     print(f"Read parquet file with {lines_in_file} lines from topic: {consume_topic}" )
+    print(f"Producing from line: {line_number}")
     for index,row in df.iterrows():
+          if index < line_number:
+                break
+
           # We dump the row to json and load to get the correct time format
           payload_string = row.to_json()
           payload = json.loads(payload_string)
           producer.send(topic=produce_topic, value=payload)
-          # We sleep 1 second to simulate scada event stream
+          producer.send(topic=topic_line_offset,value=index)
+          offset_line_consumer.commit(index)
+          # We sleep to simulate scada event stream
           sleep(consume_interval)
     print(f"Finished writing {lines_in_file} events to topic: {produce_topic}")
